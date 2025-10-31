33074f95d41f8284152e2dab59712830dde5eaa4
discon3ct <hayes.techlife@gmail.com>
2025-10-31 18:47:13 -0500

    add logging and channel entity event listner

--- src/tele_watch.py
+++ src/tele_watch.py
@@ -1,5 +1,6 @@
 from telethon import TelegramClient, events
 from dotenv import load_dotenv
+import logging
 import asyncio
 import os
 
@@ -8,18 +9,27 @@ load_dotenv()
 API_ID=os.environ.get("API_ID")
 API_HASH=os.environ.get("API_HASH")
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger=logging.getLogger(__name__)
+
 async def main():
     client=TelegramClient('sesh',API_ID,API_HASH)
 
+    await client.start()
+    channel_entity=await client.get_entity("ENTITY")
+
     @client.on(events.NewMessage)
     async def handle_message(event):
-        print(event.message.text)
+        sender=await event.get_sender()
+        channel=await event.get_chat()
+        message=event.raw_text
 
-    await client.start()
+        text=f"Telegram Alert\nChannel: {(channel.title)}\nUser: {sender.username}\nMessage: {message}\n"
+        logger.info(f"Message received from {sender.username} in {channel.title}: {message}")
+        print(text)
     await client.run_until_disconnected()
-    
-    return handle_message
 
+    return handle_message
 asyncio.run(main())
 
 
