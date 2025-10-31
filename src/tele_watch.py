ed7a996bfa5fa1ef6918f34a3e0d616b8284eaaf
discon3ct <hayes.techlife@gmail.com>
2025-10-31 15:48:25 -0500

    add for conversion to async and new message event listener

--- src/tele_watch.py
+++ src/tele_watch.py
@@ -1,5 +1,6 @@
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 from dotenv import load_dotenv
+import asyncio
 import os
 
 load_dotenv()
@@ -7,5 +8,19 @@ load_dotenv()
 API_ID=os.environ.get("API_ID")
 API_HASH=os.environ.get("API_HASH")
 
-with TelegramClient('sesh',API_ID,API_HASH) as client:
-    client.loop.run_until_complete(client.send_message('me',"Testing Connection"))
+async def main():
+    client=TelegramClient('sesh',API_ID,API_HASH)
+
+    @client.on(events.NewMessage)
+    async def handle_message(event):
+        print(event.message.text)
+
+    await client.start()
+    await client.run_until_disconnected()
+    
+    return handle_message
+
+asyncio.run(main())
+
+
+
